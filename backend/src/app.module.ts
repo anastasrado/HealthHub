fa8974b2800ebda5a409b6cc83965d2020541ed2
anastasorigo <anastas.radosavljevic@origo.is>
2024-08-08 17:47:11 +0200

    Implemented postgres, swagger and orm initial setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
-import { RequestLoggerMiddleware } from './middleware/request-logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [
     {
