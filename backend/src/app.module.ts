e84d2df8a2a360257912203f48bd3e41aaf231bf
anastasorigo <anastas.radosavljevic@origo.is>
2024-08-08 16:38:25 +0200

    Initial setup  for loggin with winston and error handling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { RequestLoggerMiddleware } from './middleware/request-logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    AppService,
+    Logger,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  // let's add a middleware on all routes
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
